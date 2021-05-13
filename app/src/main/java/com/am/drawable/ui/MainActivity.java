82667bc23bd5feed1df1a3b2d4438673776d9213
Alex <moferalex@gmail.com>
2021-05-13 23:45:08 +0800

    [update] 发布2.1.0版本

---
+++ app/src/main/java/com/am/drawable/ui/MainActivity.java
@@ -0,0 +1,10 @@
+package com.am.drawable.ui;
+
+import com.am.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public MainActivity() {
+        super(R.layout.activity_main);
+    }
+}
